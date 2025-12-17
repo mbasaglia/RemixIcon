f6dfad0aaf4580f6c3178db4ef4a5d09a5c429cc
Mattia "Glax" Basaglia <dev@dragon.best>
2025-12-17 11:22:19 +0100

    Add custom icons

--- fantasticonrc.js
+++ fantasticonrc.js
@@ -6,13 +6,22 @@ module.exports = {
     name: "glicons",
 
     // Font types to generate
-    fontTypes: ["woff2", "woff", "ttf", "eot", "svg"],
+    fontTypes: ["woff2", "woff", "ttf"],
 
     // Asset types to generate
     assetTypes: ["css", "html", "json"],
 
     // CSS options
     prefix: "ri",
+    tag: "",
+
+    // Fixed codepoints
+    codepoints: {
+        "arrow-down-s-fill": 0xea4d
+    },
+
+    descent: 0,
+    fontHeight: 24,
 
     getIconId(opts) { return opts.basename; },
 };
